4797668b262d8ddc14614c85ad9b888cd6d43fda
Daria Skvortsova <dariskvortsova@ozon.ru>
2024-10-09 22:51:05 +0300

    add security profiles

--- soa-lab-2-back/grammy-service/src/main/java/ru/itmo/soa/grammyservice/security/SecurityConfig.java
+++ soa-lab-2-back/grammy-service/src/main/java/ru/itmo/soa/grammyservice/security/SecurityConfig.java
@@ -1,21 +1,43 @@
 package ru.itmo.soa.grammyservice.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Разрешенные источники
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // Разрешенные методы
+        configuration.setAllowCredentials(true); // Разрешение креденшилей
+        configuration.setMaxAge(3600L); // Время жизни предустановленных заголовков
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize ->
-                        authorize
-                                .anyRequest().permitAll()
-                )
-                .csrf(csrf -> csrf.disable());
+        http.cors(httpSecurityCorsConfigurer ->
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()));
+        http
+            .authorizeHttpRequests(authorize ->
+                    authorize
+                            .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
 
 
         return http.build();
--- soa-lab-2-back/main-service/src/main/java/ru/itmo/soa/mainservice/security/SecurityConfig.java
+++ soa-lab-2-back/main-service/src/main/java/ru/itmo/soa/mainservice/security/SecurityConfig.java
@@ -6,18 +6,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Разрешенные источники
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // Разрешенные методы
+        configuration.setAllowCredentials(true); // Разрешение креденшилей
+        configuration.setMaxAge(3600L); // Время жизни предустановленных заголовков
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize ->
-                        authorize
-                                .anyRequest().permitAll()
-                )
-                .csrf(csrf -> csrf.disable());
+        http.cors(httpSecurityCorsConfigurer ->
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()));
+        http
+            .authorizeHttpRequests(authorize ->
+                    authorize
+                            .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
+
 
         return http.build();
     }
 }
-
