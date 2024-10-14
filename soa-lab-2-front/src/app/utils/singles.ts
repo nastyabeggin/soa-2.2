0acc81e3f7e6281452d0b0e3597a181fc79bbeed
Daria Skvortsova <dariskvortsova@ozon.ru>
2024-10-14 15:00:53 +0300

    fix quotes

--- soa-lab-2-front/src/app/utils/singles.ts
+++ soa-lab-2-front/src/app/utils/singles.ts
@@ -1,11 +1,12 @@
 import {Single} from "@/app/types/single";
 
 export function convertStringToSingles(text: string) {
-    if (text.length && text.indexOf('"') === -1) throw new Error("Please use brackets to enter singles");
+    const quotes = text.match(/"/g)?.length;
+    if (text.length && !quotes) throw new Error("Please use quotes to enter singles");
 
     let singles = text.split(new RegExp(/"\s*,/));
 
-    if (singles.length !== text.match(/"/)?.length) throw new Error("Please use brackets fo each single twice");
+    if (singles.length * 2 !== quotes) throw new Error("Please use quotes for each single twice");
 
     singles = singles.map((single) => single.trim().replace(new RegExp(/^"/), '').replace(new RegExp(/"$/), ''));
     const result: Single[] = singles.map((single) => ({name: single}));
